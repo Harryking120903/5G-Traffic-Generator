94243ef78f61bd63252bbe7bb9dbcd8df08cb8ed
Harryking120903 <100433794+Harryking120903@users.noreply.github.com>
2025-02-10 15:29:32 +0530

    Update main.py

--- 5GT-GAN/main.py
+++ 5GT-GAN/main.py
@@ -1,11 +1,13 @@
 import hydra
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
+from hydra.utils import get_original_cwd
+import os
 
-
-@hydra.main(config_path='config', config_name='config')
+@hydra.main(config_path='config', config_name='config',version_base=None)
 def train(cfg):
     # Load Model with config file
+    traffic_data_path = os.path.join(get_original_cwd(), "5GT-GAN/data/Traffics.csv")
     model = hydra.utils.instantiate(cfg.model)
 
     # Set Checkpoint config
@@ -17,7 +19,7 @@ def train(cfg):
     )
 
     # Set Train options
-    trainer = Trainer(gpus=[0], log_every_n_steps=1,
+    trainer = Trainer(accelerator="auto",devices="auto", log_every_n_steps=1,
                       num_sanity_val_steps=0, max_epochs=-1, callbacks=[ckpt_callback])
 
     # Load datamodule
